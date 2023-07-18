c9107a4e8e824c1a5d6782e745269e67f86248a6
Cristian Villa <c.villavirgen@gmail.com>
2023-07-18 16:49:50 -0600

    Set up project with webpack, created basic HTML structure, added initial JavaScript functionality, and styled with CSS

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 import './style.css';
+import '@fortawesome/fontawesome-free/css/all.css';
 
-console.log('Hello webpack!');
+const tasks = [
+  {
+    description: 'Task 1',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'Task 2',
+    completed: false,
+    index: 2,
+  },
+  {
+    description: 'Task 3',
+    completed: false,
+    index: 3,
+  },
+];
+
+const printTask = () => {
+  const tasksContainer = document.getElementById('tasks-list');
+
+  // Sort tasks array based on index property
+  tasks.sort((a, b) => a.index - b.index);
+
+  tasks.forEach((task) => {
+    const listItem = document.createElement('li');
+    listItem.innerHTML = `
+      <input type="checkbox" ${task.completed ? 'checked' : ''}>
+      <p>${task.description}</p>
+    `;
+    tasksContainer.appendChild(listItem);
+  });
+};
+
+window.addEventListener('load', printTask);
