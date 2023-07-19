92cd61adb276beddb312fd30b6c00ebd6628ffd0
Cristian Villa <c.villavirgen@gmail.com>
2023-07-19 14:37:14 -0600

    Solve some errors in functionality in .tasks-list and .delete-button

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './style.css';
 import '@fortawesome/fontawesome-free/css/all.css';
-import { addTask, saveTasks, loadTasks } from './taskFunctions.js';
+import { addTask, deleteTask, saveTasks, loadTasks } from './taskFunctions.js';
 
 let tasks = loadTasks();
 
@@ -16,17 +16,40 @@ const printTask = () => {
     listItem.innerHTML = `
       <input type="checkbox" ${task.completed ? 'checked' : ''}>
       <p>${task.description}</p>
+      <button class="delete-button" data-index="${task.index}">
+        <i class="fas fa-trash"></i>
+      </button>
     `;
     tasksContainer.appendChild(listItem);
+  });
+
+  // Add event listeners to the delete buttons
+  const deleteButtons = document.querySelectorAll('.delete-button');
+  deleteButtons.forEach((button) => {
+    button.addEventListener('click', (event) => {
+      let target = event.target;
+      // If the target is the icon, use its parent node (the button) instead
+      if (target.tagName === 'I') {
+        target = target.parentNode;
+      }
+      const index = parseInt(target.dataset.index, 10);
+      deleteTask(tasks, index);
+      saveTasks(tasks); // Save tasks to local storage
+      printTask(); // Update the tasks list
+    });
   });
 };
 
 window.addEventListener('load', printTask);
 
 // Add a new task
-addTask(tasks, 'New Task');
-saveTasks(tasks); // Save tasks to local storage
+const taskInput = document.getElementById('task-input');
+const taskSubmitButton = document.getElementById('task-submit-button');
 
-// Reload tasks from local storage and update the DOM
-tasks = loadTasks();
-printTask();
+taskSubmitButton.addEventListener('click', (event) => {
+  event.preventDefault(); // Prevent form submission
+  addTask(tasks, taskInput.value);
+  saveTasks(tasks); // Save tasks to local storage
+  taskInput.value = ''; // Clear the input field
+  printTask(); // Update the tasks list
+});
