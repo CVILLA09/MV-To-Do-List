8b60434cb8ab71d4cb12d4dc90653bc7d21cc1b7
Nessrine88 <macherki.nessrine@yahoo.com>
2023-07-27 18:26:05 +0100

    add testing for update and clearComplete functions

---
+++ src/statusUpdates.test.js
@@ -0,0 +1,32 @@
+//statusUpdate test
+
+import { updateStatus, clearCompleted } from './statusUpdates.js';
+
+describe('updateStatus', () => {
+  test('updates a task\'s status', () => {
+
+    const tasks=[ 
+      {description: "task1 ", completed:false , index:1},
+      {description: "task2 ", completed:true , index:2}
+    ];
+updateStatus(tasks,true,1);
+expect(tasks[0].completed).toBe(false);
+  })
+});
+
+describe('clearCompleted', () => {
+  test('Clears all completed tasks', () => {
+   
+    const tasks=[ 
+      {description: "Task 1", completed:true , index:1},
+      {description: "Task 2", completed:false , index:2}
+    ];
+    const result = clearCompleted(tasks);
+    expect(result).toEqual([{ description: "Task 2", completed: false, index:2 }]);
+  });
+  });
+
+
+
+
+
