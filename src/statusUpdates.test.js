b0c07cadf3999573b3f996f1abd71c153ccff956
Cristian Villa <c.villavirgen@gmail.com>
2023-07-27 11:37:49 -0600

    Fix linters - details

--- src/statusUpdates.test.js
+++ src/statusUpdates.test.js
@@ -1,32 +1,25 @@
-//statusUpdate test
+// statusUpdate test
 
 import { updateStatus, clearCompleted } from './statusUpdates.js';
 
 describe('updateStatus', () => {
   test('updates a task\'s status', () => {
-
-    const tasks=[ 
-      {description: "task1 ", completed:false , index:1},
-      {description: "task2 ", completed:true , index:2}
+    const tasks = [
+      { description: 'task1 ', completed: false, index: 1 },
+      { description: 'task2 ', completed: true, index: 2 },
     ];
-updateStatus(tasks,true,1);
-expect(tasks[0].completed).toBe(false);
-  })
+    updateStatus(tasks, true, 1);
+    expect(tasks[0].completed).toBe(false);
+  });
 });
 
 describe('clearCompleted', () => {
   test('Clears all completed tasks', () => {
-   
-    const tasks=[ 
-      {description: "Task 1", completed:true , index:1},
-      {description: "Task 2", completed:false , index:2}
+    const tasks = [
+      { description: 'Task 1', completed: true, index: 1 },
+      { description: 'Task 2', completed: false, index: 2 },
     ];
     const result = clearCompleted(tasks);
-    expect(result).toEqual([{ description: "Task 2", completed: false, index:2 }]);
+    expect(result).toEqual([{ description: 'Task 2', completed: false, index: 2 }]);
   });
-  });
-
-
-
-
-
+});
