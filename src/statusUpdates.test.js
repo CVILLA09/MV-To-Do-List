e29c0c80ab75e9d41528cf7dff0af6cd128119e8
Nessrine88 <macherki.nessrine@yahoo.com>
2023-07-27 18:19:22 +0100

    clearcompleted function test

--- src/statusUpdates.test.js
+++ src/statusUpdates.test.js
@@ -4,6 +4,7 @@ import { updateStatus, clearCompleted } from './statusUpdates.js';
 
 describe('updateStatus', () => {
   test('updates a task\'s status', () => {
+
     const tasks=[ 
       {description: "task1 ", completed:false , index:1},
       {description: "task2 ", completed:true , index:2}
@@ -12,6 +13,19 @@ updateStatus(tasks,true,1);
 expect(tasks[0].completed).toBe(false);
   })
 });
+
+describe('clearCompleted', () => {
+  test('Clears all completed tasks', () => {
+   
+    const tasks=[ 
+      {description: "Task 1", completed:true , index:1},
+      {description: "Task 2", completed:false , index:2}
+    ];
+    const result = clearCompleted(tasks);
+    expect(result).toEqual([{ description: "Task 2", completed: false, index:2 }]);
+  });
+  });
+
 
 
 
